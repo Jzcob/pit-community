e774aa2d6e23ca3c506c082e0ec7c962dae782ca
Jzcoob Gamer <jzcoob@gmail.com>
2023-12-05 16:49:15 -0500

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -69,15 +69,15 @@ class general(commands.Cog):
     
     @app_commands.command(name="edit-embed", description="Edit an embed.")
     @app_commands.checks.has_any_role(config.moderator, config.administrators, config.transparent_admin, config.true_admin)
-    async def editEmbed(self, interaction: discord.Interaction, message_id: str, title: str, message: str, image: discord.Attachment=None, custom_color: str=None, channel: discord.TextChannel = None):
+    async def editEmbed(self, interaction: discord.Interaction, message_id: str, title: str, message: str, image: discord.Attachment=None, custom_color: discord.Color=None, channel: discord.TextChannel = None):
         try:
             if custom_color == None:
-                color = "00ff00"
+                color = 0x00ff00
             else:
                 color = custom_color
             if channel == None:
                 channel = interaction.channel
-                embed = discord.Embed(title=title, description=message, color=f"0x{color}")
+                embed = discord.Embed(title=title, description=message, color=color)
                 if image == None:
                     pass
                 else:
