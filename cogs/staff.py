cefa746dff6036a400a900b0b0dfd409d8cc4a36
Jzcoob Gamer <jzcoob@gmail.com>
2024-02-12 21:46:41 -0500

    Update staff.py

--- cogs/staff.py
+++ cogs/staff.py
@@ -14,11 +14,11 @@ class Staff(commands.Cog):
     @app_commands.command(name="staff", description="Get the staff of the server")
     async def staff(self, interaction: discord.Interaction):
         embed = discord.Embed(title="Staff", description="These are the staff members of the server.", color=0x00ff00)
-        embed.add_field(name="Owner", value="<@349719693264814092>", inline=True)
-        embed.add_field(name="Admin", value="<@306570665694199809>", inline=True)
-        embed.add_field(name="Developer", value=f"<@920797181034778655>", inline=True)
-        embed.add_field(name="Moderator's", value="<@238762194320228352> <@403705883097563139> <@615124610379284481> <@422512146065391625>", inline=True)
-        embed.add_field(name="Other Staff", value="<@631168537137905684>", inline=True)
+        embed.add_field(name="Owner", value="<@349719693264814092>", inline=False)
+        embed.add_field(name="Admin", value="<@306570665694199809>", inline=False)
+        embed.add_field(name="Developer", value=f"<@920797181034778655>", inline=False)
+        embed.add_field(name="Moderator's", value="<@238762194320228352> <@403705883097563139> <@615124610379284481> <@422512146065391625>", inline=False)
+        embed.add_field(name="Other Staff", value="<@631168537137905684>", inline=False)
         await interaction.response.send_message(embed=embed)
 
 async def setup(bot):
